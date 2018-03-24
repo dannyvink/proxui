588c752109c150c33b338e3da4914b84ef93af73
Danny Vink <danny@dannyvink.com>
2018-03-24 00:25:05 -0700

    Display the last update check timestamp

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -8,9 +8,11 @@
         <scanner v-bind:prompt-clone="promptClone" v-bind:add-message="addMessage" />
     </div>
 
-    @if ($updateable)
-	<div class="text-center mt-4">
-            Update Available!<br /><a href="/update" class="btn btn-warning btn-sm">Perform Update</a>
-        </div>
-    @endif
+    <div class="text-center mt-4">
+	@if ($updateable)
+	    Update Available!<br /><a href="/update" class="btn btn-warning btn-sm">Perform Update</a>
+	@else
+	    Last Check For Update: {{ $last_update_check }}
+	@endif
+    </div>
 @endsection
