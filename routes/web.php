062ef86f287d1a76b81678f5158d8c1b3805c01d
Danny Vink <danny@dannyvink.com>
2019-01-07 21:18:55 -0800

    Fix update check & output

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,22 @@ Route::get('/', function () {
     $updateable = false;
     if (!session()->has('checked_for_update')) {
         shell_exec('git fetch');
-        $result = shell_exec('git status');
-        if (preg_match("/behind (.+) by/im", $result)) {
-            $updateable = true;
-        }
-        session(['checked_for_update' => date("Y-m-d H:i:s")]);
     }
+    $result = shell_exec('git status');
+    if (preg_match("/behind (.+) by/im", $result)) {
+        $updateable = true;
+    }
+    session(['checked_for_update' => date("Y-m-d H:i:s")]);
     return view('scan', ['updateable' => $updateable, 'last_update_check' => session('checked_for_update')]);
 });
 
 Route::get('/update', function() {
-    $pull = shell_exec('cd ' . base_path() . ' && git pull');
-    $npm = shell_exec('cd ' . base_path() . ' && npm run prod');
-    $migrate = shell_exec('cd ' . base_path() . ' && php artisan migrate');
+    $directory = getcwd();
+    chdir(base_path());
+    $pull = shell_exec('git pull');
+    $npm = shell_exec('npm run prod');
+    $migrate = shell_exec('php artisan migrate');
+    chdir($directory);
     return '<pre>' . $pull . '</pre><pre>' . $npm . '</pre><pre>' . $migrate . '</pre><a href="/">Done</a>';
 });
 
